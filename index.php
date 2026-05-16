c5105314201e797035e31a316f36d1b52ac574cd
Abraham <abrahamramos.info@gmail.com>
2026-05-16 00:41:09 -0700

    Update database connection to use environment variables

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
+require_once 'lib/env.php';
 // Database credentials
-$host    = 'localhost';
-$db      = 'test';
-$user    = 'test';
-$pass    = 'testpassword';
+$host    = $_ENV['DB_HOST'];
+$db      = $_ENV['DB_NAME'];
+$user    = $_ENV['DB_USER'];
+$pass    = $_ENV['DB_PASS'];
 $charset = 'utf8mb4';
 
 // Data Source Name
